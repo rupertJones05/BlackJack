f195c4d1c0b505724b4c3a9fa3f385159f7ef9ec
rupertJones05 <156025197+rupertJones05@users.noreply.github.com>
2024-02-29 10:35:28 -0800

    Update BlackJack.java

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -27,7 +27,9 @@ public class BlackJack {
         dealer.add(deck.getCard());
         player.add(deck.getCard());
         dealer.add(deck.getCard());
-        printAll();
+        System.out.println("Dealer's Hand: " + dealer.getFirst() + " ?");
+        printPlayer();
+        addCardsValue();
         System.out.println();
     }
 
@@ -61,43 +63,107 @@ public class BlackJack {
         System.out.println("");
     }
 
+    boolean runAgain = false;;
+                             
     public void hitOrStand() {
         Scanner kb = new Scanner(System.in);
-        System.out.println("Would you like to hit or stand?");
 
-        if((kb.nextLine().equals("hit"))) {
-            player.add(deck.getCard());
-            printAll();
-            addCardsValue();
-        }
+        while(true) {
+            System.out.println("Would you like to hit or stand?");
+            String userInput = kb.nextLine().toLowerCase();
 
+            if(userInput.equals("hit")) {
+                player.add(deck.getCard());
+                printAll();
+                addCardsValue();
 
+                if(playerValue > 21) {
+                    System.out.println("You've Lost!");
+                    break;
+                }
+            } else if(userInput.equals("stand")) {
+                break;
+            } else {
+                System.out.println("Please enter a valid input of 'hit' or 'stand'.");
+            }
+        }  
     }
 
-    public void checkValue() {
+    public void checkResult() {
         if(playerValue > 21) {
-            ""
+            System.out.println("You've lost");
+        } else if(dealerValue > 21) {
+            System.out.println("You've won");
+        } else if(playerValue > dealerValue) {
+            System.out.println("You've won");
+        } else if(dealerValue > playerValue) {
+            System.out.println("You've lost");
+        } else {
+            System.out.println("You've both drawn");
         }
     }
+        
+            
 
     public void addCardsValue() {
         dealerValue = 0;
+        int numberOfAces;
         for (int i = 0; i < dealer.size(); i++) {
             dealerValue += dealer.get(i).getCardValue();
+
+            if(dealer.get(i).equals("A")){
+                numberOfAces++;
+            }
         }
+        
+        while(numberOfAces > 0 && dealerValue > 21) {
+            dealerValue -= 10;
+            numberOfAces--;
+        }
+        
         System.out.println("Dealer's total value = " + dealerValue);
+        
 
         playerValue = 0;
         for (int i = 0; i < player.size(); i++) {
             playerValue += player.get(i).getCardValue();
+            if(player.get(i).equals("A")){
+                numberOfAces++;
+            }
+        }
+        
+        while(numberOfAces > 0 && playerValue > 21) {
+            playerValue -= 10;
+            numberOfAces--;
         }
         System.out.println("Player's total value = " + playerValue);
 
     }
 
     private void run() {
-        deck.shuffle();
-        dealCards();
-        hitOrStand();
+        while(true) { 
+            deck.shuffle();
+            player.clear();
+            dealer.clear();
+            
+            dealCards();
+            
+            hitOrStand();
+
+            while(dealerValue < 17) {
+                dealer.add(deck.getCard());
+                addCardsValue();
+            }
+            printAll():
+            checkResult();
+
+            System.out.println("Would you like to play another round? (type 'yes' or 'no')");
+            String userInput2 = kb.nextLine().toLowerCase();
+            
+            if(userInput2.equals("no") {
+                System.out.println("Thank you for playing!");
+                break;
+            }
+        }
     }
 }
